e2426ad1714ce0ad6d4005016c57fb82b1e7324a
eoghanroberts <30303258+eoghanroberts@users.noreply.github.com>
2017-11-16 15:32:23 +1100

    Uploading tests.
    Not sure which parts are actually needed

---
+++ TreeSystemTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TreeSystemTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TreeSystemTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("75fdb72a-e304-40a4-b956-613a36beeb9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TreeSystemTests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TreeSystemTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NumberOfNodesTest()
+        {
+            Node Node = new Node();
+            Node.AddNodes(4);
+            List<List<int>> NodesTest = Node.ReturnNodes();
+            int Actual;
+            Actual = NodesTest.Count;
+            double Expected = 8;
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod]
+        public void PredictionTest()
+        {
+            Node Node = new Node();
+            Node.TreeCreator(4);
+            List<List<int>> PredictionTest = Node.ReturnNodes();
+            for(int i = 0; i < 15; i++)
+            {
+                PredictionTest[i][2] = 1;
+            }
+            double Actual = Node.PredictSystem(PredictionTest, 4);
+            double Expected = 1;
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod]
+        public void NumberOfContainersTest()
+        {
+            Container Container = new Container();
+            Container.CreateContainers(4);
+            List<List<int>> ContainersTest = Container.ReturnContainer();
+            int Actual;
+            Actual = ContainersTest.Count;
+            double Expected = 16;
+            Assert.AreEqual(Expected, Actual);
+        }
+        [TestMethod]
+        public void SystemRunTest()
+        {
+            Node Node = new Node();
+            Node.TreeCreator(4);
+            Container Container = new Container();
+            Ball ballModel = new Ball();
+            Container.CreateContainers(4);
+            ballModel.RunSystem(Node.ReturnNodes(), 4, Container.ReturnContainer());
+            int Empty = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                if (Container.ReturnContainer()[i][1] == 0)
+                {
+                    Empty++;
+                }
+            }
+            Assert.AreEqual(1, Empty);
+        }
+    }
+}
